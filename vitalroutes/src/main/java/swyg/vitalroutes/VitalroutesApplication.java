ea57e52d3122386f8e860098e23e3c9100524a90
Hany-Kim <hanykim1210@gmail.com>
2024-02-16 03:29:03 +0900

    etc : 초기 Spring boot server 세팅

---
+++ vitalroutes/src/main/java/swyg/vitalroutes/VitalroutesApplication.java
@@ -0,0 +1,13 @@
+package swyg.vitalroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalroutesApplication.class, args);
+	}
+
+}
