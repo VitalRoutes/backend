4c267820f2cb9d703fb3afa6b429b6de5dc93559
Hany-Kim <hanykim1210@gmail.com>
2024-02-16 13:41:43 +0900

    etc : spring boot 서버 세팅

---
+++ vitalroutes/src/test/java/swyg/vitalroutes/VitalroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package swyg.vitalroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
