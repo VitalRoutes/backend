c32999fc17182bb7cd3bf2c72df85711e01278d0
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-02-20 15:06:36 +0900

    files : s3 패키지 파일 생성

--- vitalroutes/src/main/java/swyg/vitalroutes/s3/S3Config.java
+++ vitalroutes/src/main/java/swyg/vitalroutes/s3/S3Config.java
@@ -1,5 +1,13 @@
 package swyg.vitalroutes.s3;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class S3Config {
 
--- vitalroutes/src/main/java/swyg/vitalroutes/s3/S3UploadService.java
+++ vitalroutes/src/main/java/swyg/vitalroutes/s3/S3UploadService.java
@@ -1,5 +1,17 @@
 package swyg.vitalroutes.s3;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor
 public class S3UploadService {
