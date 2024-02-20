67c3b3ea171bbb5d44bd2c0d68d8a0c366b6ecde
Hany-Kim <hanykim1210@gmail.com>
2024-02-20 14:47:22 +0900

    chore: build 파일 수정
    S3 세팅 관련 빌드 추가

---
+++ vitalroutes/src/main/java/swyg/vitalroutes/s3/S3Config.java
@@ -0,0 +1,25 @@
+package swyg.vitalroutes.s3;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+}
