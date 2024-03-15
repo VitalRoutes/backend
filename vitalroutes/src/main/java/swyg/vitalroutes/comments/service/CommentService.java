1128269bb06b89776eb09751cd9d3948c3d1ac00
HwaJong <hwajong1016@gmail.com>
2024-03-15 15:27:56 +0900

    Revert "Revert "fix: 숨기기 기능 삭제 및 조회 개선""
    This reverts commit 2cd54245da8cc8afd9cfdd526dc9cf6b28376477.

--- vitalroutes/src/main/java/swyg/vitalroutes/comments/service/CommentService.java
+++ vitalroutes/src/main/java/swyg/vitalroutes/comments/service/CommentService.java
@@ -13,7 +13,6 @@ import swyg.vitalroutes.comments.dto.CommentSaveDTO;
 import swyg.vitalroutes.comments.repository.CommentRepository;
 import swyg.vitalroutes.common.exception.CommentException;
 import swyg.vitalroutes.common.response.DataWithCount;
-import swyg.vitalroutes.hide.repository.HideRepository;
 import swyg.vitalroutes.member.domain.Member;
 import swyg.vitalroutes.member.repository.MemberRepository;
 import swyg.vitalroutes.participation.domain.Participation;
@@ -30,19 +29,13 @@ import static swyg.vitalroutes.common.response.ResponseType.FAIL;
 @RequiredArgsConstructor
 public class CommentService {
 
-    private final HideRepository hideRepository;
     private final CommentRepository commentRepository;
     private final ParticipationRepository participationRepository;
     private final MemberRepository memberRepository;
 
 
-    public DataWithCount<?> viewComments(Long memberId, Long participationId, Pageable pageable) {
-        List<Long> hidedComments = hideRepository.findHidedComments(memberId);
-        // 숨김처리한 게시글 혹은 댓글이 없으면 NULL 이 들어가서 아무것도 조회되지 않음
-        if (hidedComments.size() == 0) {
-            hidedComments.add(-1L);
-        }
-        Page<Comment> pagingData = commentRepository .findAllByParticipationId(participationId, hidedComments, pageable);
+    public DataWithCount<?> viewComments(Long participationId, Pageable pageable) {
+        Page<Comment> pagingData = commentRepository .findAllByParticipationId(participationId, pageable);
         Page<CommentResponseDTO> pagingDTO = pagingData.map(CommentResponseDTO::new);
 
         long count = pagingData.getTotalElements();
